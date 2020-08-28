0d2bf1f7eb4bc47ffbf5bd1399963a81a67147b2
Murilo Correia <muri.cardoso@hotmail.com>
2020-08-28 17:08:40 -0300

    title 3

--- js/index.js
+++ js/index.js
@@ -1,33 +1,36 @@
-    var btnClasses;
-    function initialize(){
-        copyright();
-        modal();
-        modalTerms();
-        modalPolicy()
-        getJSONPolicy();
-        openTabsClick();
-        execClickTabs()
-        alternateImg()
+class LandingDevotee{
+    constructor(){
+        this.initialize()
+        this.btnClasses;
     }
-    initialize()
-    function copyright(){
+    initialize(){
+        this.copyright();
+        this.modal();
+        this.modalTerms();
+        this.modalPolicy()
+        this.getJSONPolicy();
+        this.openTabsClick();
+        this.execClickTabs()
+        this.alternateImg()
+    }
+    copyright(){
         let copyright = document.querySelector('#copyright')
         copyright.innerHTML=` Copyright © ${new Date().getFullYear()} Devotee. Todos direitos reservados.`
     }
-    function modalPolicy(){
+    modalPolicy(){
         let modalId = document.querySelector("#my-modal-policy");
         let btn = "my-btn-policy";
         let close = "close-policy";
-        modal(modalId, btn, close)
+        this.modal(modalId, btn, close)
     }
-    function modalTerms(){
+    modalTerms(){
         let modalId = document.querySelector("#my-modal-terms");
         let btn = "my-btn-terms";
         let close = "close-terms";
-        modal(modalId, btn, close)
+        this.modal(modalId, btn, close)
     }
-    function modal(modalId, btn, close) {  
-        document.addEventListener('click', function(e) {
+    modal(modalId, btn, close) {  
+        document.addEventListener('click', (e) =>{
             let targetClass = e.target.classList.contains(btn);
             let targetClassClose = e.target.classList.contains(close);
             if(targetClass){
@@ -38,9 +41,9 @@
             }
 		});
     }
-    function execClickTabs(){
+    execClickTabs(){
         let btnClass = [];
-        document.addEventListener('click', function(e) {
+        document.addEventListener('click', (e) =>{
             let targetClass = e.target.getAttribute('data-click');
             switch (targetClass) {
                 case 'click-terms':
@@ -62,41 +65,41 @@
                     break;
             }
         });  
-        openTabsClick(btnClass)
+        this.openTabsClick(btnClass)
     }
     
-    function openTabsClick(btnClass){
-        document.addEventListener('click', function (e) {     
+    openTabsClick(btnClass){
+        document.addEventListener('click', (e) =>{     
             let targetClass = e.target.classList.contains(btnClass);
             if(targetClass){
-                hideTabs()
-                openTabs(e, btnClass)
-                getJSONPolicy()
+                this.hideTabs()
+                this.openTabs(e, btnClass)
+                this.getJSONPolicy()
             }     
 		});
     }
-    function hideTabs(){
+    hideTabs(){
         let i, tabcontent
         tabcontent = document.getElementsByClassName("tabcontent");
         for (i = 0; i < tabcontent.length; i++) {
           tabcontent[i].style.display = "none";
         }
     }
-    function alternateImg(){
+    alternateImg(){
         if(window.innerWidth >= '1250'){
            document.getElementById('phone').src = './assets/iPhone@2x.png'     
         }
       
         
     }
-    function openTabs(evt, cityName) {  
+    openTabs(evt, cityName) {  
         let i, tabcontent,tablinks;
         tabcontent = document.getElementsByClassName("tabcontent");
         for (i = 0; i < tabcontent.length; i++) {
           tabcontent[i].style.display = "none";
         }
         tablinks = document.getElementsByClassName("tablinks");
-        styleTextTabs(evt,'1')
+        this.styleTextTabs(evt)
         for (i = 0; i < tablinks.length; i++) {
           tablinks[i].className = tablinks[i].className.replace(" active", "");
         }
@@ -104,27 +107,22 @@
         document.getElementById(cityName).style.display = "block";
         evt.currentTarget.className += " active";
       }
-    function styleTextTabs(evt,analyze){
-        if(analyze == '1'){
-            let tabContent,tabs;
-            let elementClicked = evt.composedPath()[1];
-            console.log(evt, elementClicked)
-            
-            tabContent = document.getElementsByClassName("content-terms");
-            tabs = document.getElementsByClassName("tab");
-            for (let i = 0; i < tabContent.length; i++) {
-                tabContent[i].classList.remove('modal-terms'); 
-            }
-            for (let e = 0; e < tabContent.length-6; e++) {
-                if(elementClicked.classList.value == tabs[0].children[e].classList.value){
-                    tabs[0].children[e].classList.add('modal-terms');
-                }
-             
-            }
+    styleTextTabs(evt){
+        let tabContent,tabs;
+        let elementClicked = evt.composedPath()[1];
+        tabContent = document.getElementsByClassName("content-terms");
+        tabs = document.getElementsByClassName("tab");
+        for (let i = 0; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('modal-terms'); 
         }
-        
+        for (let e = 0; e < tabContent.length-6; e++) {
+            if(elementClicked.classList.value == tabs[0].children[e].classList.value){
+                tabs[0].children[e].classList.add('modal-terms');
+            }
+         
+        }
     }
-    function getJSONPolicy(){
+    getJSONPolicy(){
         let policyPivacy = JSON.parse(dataprivacy);
         let terms = JSON.parse(dataterms);
         let cookies = JSON.parse(datacookies);
@@ -136,3 +134,4 @@
         document.getElementById('procedure').innerHTML = procedure[0].content;
         document.getElementById('security').innerHTML = security[0].content;
     }
+}
