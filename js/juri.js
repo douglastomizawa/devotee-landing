ef512f8debab2052157d324b88df24823565c76e
douglastomizawa <tomizawadouglas@gmail.com>
2020-09-02 14:52:24 -0300

    Correção da tradução

--- js/juri.js
+++ js/juri.js
@@ -1,45 +1,44 @@
 
-    function initialize(){
-        verifyLanguage();
-    }
-    initialize()
-    function verifyLanguage(){
-        let dataTraduction = data;
+function initialize() {
+    verifyLanguage();
+}
+initialize()
+function verifyLanguage() {
+    let dataTraduction = data;
 
-        // var userLang = navigator.language;
-        let location = window.location.href ;
-        console.log(data)
-        switch (location) {
-            case 'devotee.com.br':
-                traductionLanguage(dataTraduction[0].pt);
-                break;
-            case 'devotee.network':
-                traductionLanguage(dataTraduction[0].us);
+    // var userLang = navigator.language;
+    let location = window.location.hostname;
+    // console.log(data)
+    switch (location) {
+        case location = 'devotee.com.br':
+            traductionLanguage(dataTraduction[0].pt);
             break;
-        
+        case location = 'devotee.network':
+            traductionLanguage(dataTraduction[0].us);
+            break;
+
+        default:
+            traductionLanguage(dataTraduction[0].us);
+            break;
+    }
+}
+function traductionLanguage(data) {
+
+    for (let i = 0; i < Object.values(data).length; i++) {
+        let objectKeys = Object.keys(data)[i],
+            objectValue = Object.values(data)[i],
+            elementIds = document.getElementById(objectKeys);
+
+
+        switch (!elementIds) {
+            case false:
+                document.title = objectValue.title
+                elementIds.innerHTML = objectValue.text;
+
+                break;
             default:
-                traductionLanguage(dataTraduction[0].us);
                 break;
         }
     }
-    function traductionLanguage(data){
-       
-        for (let i = 0; i < Object.values(data).length; i++) {
-            let objectKeys = Object.keys(data)[i],
-            objectValue = Object.values(data)[i],
-            elementIds =  document.getElementById(objectKeys);
-            
-            
-            switch (!elementIds) {
-                case false:
-                    document.title = objectValue.title
-                    elementIds.innerHTML = objectValue.text;
 
-                    break;      
-                default:
-                    break;
-            }
-        }
-        
-    }
-   
+}
