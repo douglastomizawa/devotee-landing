c8cb7b7a4ba14458590d7f5916254daf02d7e9e0
Murilo Correia <muri.cardoso@hotmail.com>
2020-09-02 12:29:16 -0300

    add news files

--- js/juri.js
+++ js/juri.js
@@ -5,42 +5,41 @@
     initialize()
     function verifyLanguage(){
         let dataTraduction = data;
-        console.log(data)
+
         // var userLang = navigator.language;
         let location = window.location.href ;
+        console.log(data)
         switch (location) {
             case 'devotee.com.br':
-               
                 traductionLanguage(dataTraduction[0].pt);
-                copyright(dataTraduction[0].pt.copyright);
                 break;
             case 'devotee.network':
-                traductionLanguage(dataTraduction[1].us);
-                copyright(dataTraduction[1].us.copyright);
+                traductionLanguage(dataTraduction[0].us);
             break;
         
             default:
-                traductionLanguage(dataTraduction[1].us);
-                copyright(dataTraduction[1].us.copyright);
+                traductionLanguage(dataTraduction[0].us);
                 break;
         }
     }
     function traductionLanguage(data){
+       
         for (let i = 0; i < Object.values(data).length; i++) {
             let objectKeys = Object.keys(data)[i],
             objectValue = Object.values(data)[i],
             elementIds =  document.getElementById(objectKeys);
-            console.log(elementIds)
-            if(objectKeys.indexOf('alt') == -1){
-                elementIds.innerHTML = objectValue;
+            
+            
+            switch (!elementIds) {
+                case false:
+                    document.title = objectValue.title
+                    elementIds.innerHTML = objectValue.text;
+
+                    break;      
+                default:
+                    break;
             }
-            if(objectKeys.indexOf('src') != -1){
-                elementIds.src = objectValue
-            } 
         }
-    }
-    function copyright(text){
-        let copyright = document.querySelector('#copyright');
-        copyright.innerHTML=` Copyright © ${new Date().getFullYear()} ${text}`
+        
     }
    
